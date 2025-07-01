ecf2c68d41b6503c994f2a7df1efe914e2dcb5c6
Grady O'Keefe <gradyokeefe8@gmail.com>
2025-07-01 08:55:36 -0400

    sectors

--- src/fluid_sim/mod.rs
+++ src/fluid_sim/mod.rs
@@ -3,32 +3,86 @@ use rand::Rng;
 
 pub mod vec2;
 
-const GRAVITY_NUMBER: f32 = 30.;
-const PARTICLE_NUMBER: usize = 288;
+const GRAVITY_NUMBER: f32 = 50.;
+const PARTICLE_NUMBER: usize = 256;
+const MAX_START_SPEED: f32 = 140.0;
 
 #[derive(Clone, Debug, Default)]
 pub struct FluidSim {
     particles_positions: Vec<Vec2>,
     particles_velocities: Vec<Vec2>,
+    sectors: [Vec<usize>; 9],
 }
 
 impl FluidSim {
     pub fn new_rand(size: winit::dpi::PhysicalSize<u32>) -> Self {
         #[allow(deprecated)]
         let mut rng = rand::thread_rng();
+        let width = size.width;
+        let height = size.height;
 
-        let particles_positions = (0..PARTICLE_NUMBER)
+        let particles_positions: Vec<Vec2> = (0..PARTICLE_NUMBER)
             .map(|_| Vec2 {
                 #[allow(deprecated)]
-                x: rng.gen_range(0.0..size.width as f32),
+                x: rng.gen_range(0.0..width as f32),
                 #[allow(deprecated)]
-                y: rng.gen_range(0.0..size.height as f32),
+                y: rng.gen_range(0.0..height as f32),
             })
             .collect();
 
+        let particles_velocities: Vec<Vec2> = (0..PARTICLE_NUMBER)
+            .map(|_| Vec2 {
+                #[allow(deprecated)]
+                x: rng.gen_range(-MAX_START_SPEED..MAX_START_SPEED),
+                #[allow(deprecated)]
+                y: rng.gen_range(-MAX_START_SPEED..MAX_START_SPEED),
+            })
+            .collect();
+
+        let mut sectors: [Vec<usize>; 9] = Default::default();
+
+        let w_one_third: f32 = (width / 3) as f32;
+        let w_two_third: f32 = w_one_third + w_one_third;
+        let w_three_third: f32 = size.width as f32;
+        let h_one_third: f32 = (height / 3) as f32;
+        let h_two_third: f32 = h_one_third + h_one_third;
+        let h_three_third: f32 = size.height as f32;
+
+        // ignore how ugly this is I don't like it either
+        for (i, steve) in particles_positions.iter().enumerate() {
+            if steve.x < w_one_third {
+                if steve.y < h_one_third {
+                    sectors[0].push(i);
+                } else if steve.y < h_two_third {
+                    sectors[1].push(i);
+                } else if steve.y < h_three_third {
+                    sectors[2].push(i);
+                }
+            } else if steve.x < w_two_third {
+                if steve.y < h_one_third {
+                    sectors[3].push(i);
+                } else if steve.y < h_two_third {
+                    sectors[4].push(i);
+                } else if steve.y < h_three_third {
+                    sectors[5].push(i);
+                }
+            } else if steve.x < w_three_third {
+                if steve.y < h_one_third {
+                    sectors[6].push(i);
+                } else if steve.y < h_two_third {
+                    sectors[7].push(i);
+                } else if steve.y < h_three_third {
+                    sectors[8].push(i);
+                }
+            }
+        }
+
+        dbg!(&sectors);
+
         Self {
             particles_positions,
-            particles_velocities: vec![Vec2::default(); PARTICLE_NUMBER],
+            particles_velocities,
+            sectors,
         }
     }
 
